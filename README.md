d7c26b639d96e36dd92ddd35d52009ff68ec5fc1
Dror <drorvinkler@gmail.com>
2020-11-17 07:45:05 +0200

    Python package stuff

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# thornfield
+# `thornfield`: Advanced caching in python
+
+[![Build Status](https://travis-ci.com/drorvinkler/thornfield.svg?branch=master)](https://travis-ci.com/drorvinkler/yasoo)
+[![codecov](https://codecov.io/gh/drorvinkler/thornfield/branch/master/graph/badge.svg)](https://codecov.io/gh/drorvinkler/yasoo)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="thornfield",
+    version="0.1.0",
+    author="Dror A. Vinkler",
+    description="Advanced caching in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drorvinkler/thornfield",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license='MIT',
+)
